d7ac503391750f47884095fd16e43b9ca2481529
Dawid <dawidklose568@gmail.com>
2021-04-09 15:48:36 +0200

    commitDawK1

--- frames.py
+++ frames.py
@@ -1,33 +1,53 @@
 import pandas as pd
+import re
 
-file = open('DictOfNegativeWords.txt', 'w')
-frame = pd.read_csv('train.csv')
 
-dictionary = {}
-dictionary_bad = {}
-for i in range(len(frame)-1, 0, -1):
-    comment = frame.iloc[i]['comment_text']
-    x = [word.strip(':,;"-_·()[]{}\n?!@|.') for word in comment.split()]
-    bad=frame.iloc[i]['toxic']+frame.iloc[i]['severe_toxic']+frame.iloc[i]['obscene']+frame.iloc[i]['threat']+frame.iloc[i]['identity_hate']+frame.iloc[i]['insult']
-    for j in range(len(x)):
-        if x[j] != '':
-            slowo=x[j].lower()
-            if slowo in dictionary:
-                dictionary[slowo] = dictionary[slowo]+1
-                if bad>0:
-                    dictionary_bad[slowo] = dictionary_bad[slowo] + 1
-            else:
-                dictionary[slowo] = 1
-                if bad > 0:
-                    dictionary_bad[slowo] = 1
+def create_data():
+    file = open('DictOfNegativeWords.txt', 'w')
+    frame = pd.read_csv('train.csv')
+    dictionary, dictionary_bad = {}, {}
+
+    for i in range(len(frame)-1, 0, -1):
+        comment = frame.iloc[i]['comment_text']
+        x = [word.strip(':,;"-_·()[]{}\n?!@|.') for word in comment.split()]
+        bad = frame.iloc[i]['toxic']+frame.iloc[i]['severe_toxic']+frame.iloc[i]['obscene']+frame.iloc[i]['threat']+frame.iloc[i]['identity_hate']+frame.iloc[i]['insult']
+        for j in range(len(x)):
+            slowo = x[j].lower()
+            if slowo != '':
+                if slowo in dictionary:
+                    dictionary[slowo] += 1
+                    if bad > 0:
+                        dictionary_bad[slowo] += 1
                 else:
-                    dictionary_bad[slowo] = 0
+                    dictionary[slowo] = 1
+                    if bad > 0:
+                        dictionary_bad[slowo] = 1
+                    else:
+                        dictionary_bad[slowo] = 0
+    for key in dictionary.keys():
+        result = str((key, float(dictionary_bad[key]/dictionary[key])))
+        file.write(result)
+        print(result)
+    file.close()
 
-for key in dictionary.keys():
-    result = str((key, float(dictionary_bad[key]/dictionary[key])))
-    file.write(result)
-    print(result)
-file.close()
+
+def read_data_from_file():
+    file = open('DictOfNegativeWords.txt', 'r')
+    outputNegative = file.read().split(')(')
+    print(type(outputNegative))
+    outputDict = {}
+    try:
+        for i in range(len(outputNegative)):
+            #za pomocą regexów:
+                #do klucza outputDict dodaj stringa w cudzyszłowie
+                #do wartości dodaj floata
+    except:
+        print('sth went wrong')
+    return outputNegative
+
+
+print(read_data_from_file()[7])
+
 
 """
 TODO-
