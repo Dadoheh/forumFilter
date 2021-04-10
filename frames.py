9b18f4c279cce237c649aa51fcbde71131e5b625
Dawid <dawidklose568@gmail.com>
2021-04-10 23:14:59 +0200

    commit6

--- frames.py
+++ frames.py
@@ -1,33 +1,70 @@
 import pandas as pd
+import re
+import numpy as np
 
-file = open('DictOfNegativeWords.txt', 'w')
-frame = pd.read_csv('train.csv')
+def create_data():
+    file = open('DictOfNegativeWords.txt', 'w') # prawdopodobnie do wyrzucenia/ do modyfikacji
+    frame = pd.read_csv('train.csv', low_memory=True)
+    dictionary, dictionary_bad = {}, {}
+    toxicitySeries = pd.Series(index=[], dtype=float)
 
-dictionary = {}
-dictionary_bad = {}
-for i in range(len(frame)-1, 0, -1):
-    comment = frame.iloc[i]['comment_text']
-    x = [word.strip(':,;"-_·()[]{}\n?!@|.') for word in comment.split()]
-    bad=frame.iloc[i]['toxic']+frame.iloc[i]['severe_toxic']+frame.iloc[i]['obscene']+frame.iloc[i]['threat']+frame.iloc[i]['identity_hate']+frame.iloc[i]['insult']
-    for j in range(len(x)):
-        if x[j] != '':
-            slowo=x[j].lower()
-            if slowo in dictionary:
-                dictionary[slowo] = dictionary[slowo]+1
-                if bad>0:
-                    dictionary_bad[slowo] = dictionary_bad[slowo] + 1
-            else:
-                dictionary[slowo] = 1
-                if bad > 0:
-                    dictionary_bad[slowo] = 1
+    for i in range(len(frame) - 1, 0, -1):
+        comment = frame.iloc[i]['comment_text']
+        x = [word.strip(':,;"-_·()[]{}\n?!@|.') for word in comment.split()]
+        bad = (frame.iloc[i]['toxic'] + frame.iloc[i]['severe_toxic']
+               + frame.iloc[i]['obscene'] + frame.iloc[i][
+            'threat'] + frame.iloc[i]['identity_hate'] + frame.iloc[i]['insult'])
+        for j in range(len(x)):
+            slowo = x[j].lower()
+            if slowo != '':
+                if slowo in dictionary:
+                    dictionary[slowo] += 1
+                    if bad > 0:
+                        dictionary_bad[slowo] += 1
                 else:
-                    dictionary_bad[slowo] = 0
+                    dictionary[slowo] = 1
+                    if bad > 0:
+                        dictionary_bad[slowo] = 1
+                    else:
+                        dictionary_bad[slowo] = 0
+    for key in dictionary.keys():
+        #result = str((key, float(dictionary_bad[key] / dictionary[key])))
+        # file.write(result)
 
-for key in dictionary.keys():
-    result = str((key, float(dictionary_bad[key]/dictionary[key])))
-    file.write(result)
-    print(result)
-file.close()
+        toxicityRatio = float(dictionary_bad[key] / dictionary[key])
+        toxicitySeries[key] = toxicityRatio
+
+    file.close()
+    print("Długość zapisywanego słownika {}".format(len(dictionary)))
+
+    toxicitySeries.to_csv('savedWord.csv')
+    return toxicitySeries
+
+
+def read_data_from_file():
+    file = open('DictOfNegativeWords.txt', 'r')
+    outputNegative = file.read().split(')(')
+    print(type(outputNegative))
+    outputDict = {}
+
+    for i in range(len(outputNegative)):
+        temp = outputNegative[i]
+        key = re.search(r"'(.*?)'", temp)
+        variable = float(temp.split(', ')[1][:-1])
+        if key and variable:
+            key = key.group()
+            outputDict[str(key[1:-1])] = variable
+
+    return len(outputDict), outputDict
+
+
+#print(read_data_from_file())
+print(create_data())
+
+
+# def save_data_to_csv(file_csv):
+
+
 
 """
 TODO-
