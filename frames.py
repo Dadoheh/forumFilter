06ad8f9e4cdacdd8148abd4c71f18d7711c27734
Dawid <dawidklose568@gmail.com>
2021-04-10 12:34:48 +0200

    commitDawK2

--- frames.py
+++ frames.py
@@ -7,10 +7,11 @@ def create_data():
     frame = pd.read_csv('train.csv')
     dictionary, dictionary_bad = {}, {}
 
-    for i in range(len(frame)-1, 0, -1):
+    for i in range(len(frame) - 1, 0, -1):
         comment = frame.iloc[i]['comment_text']
         x = [word.strip(':,;"-_·()[]{}\n?!@|.') for word in comment.split()]
-        bad = frame.iloc[i]['toxic']+frame.iloc[i]['severe_toxic']+frame.iloc[i]['obscene']+frame.iloc[i]['threat']+frame.iloc[i]['identity_hate']+frame.iloc[i]['insult']
+        bad = frame.iloc[i]['toxic'] + frame.iloc[i]['severe_toxic'] + frame.iloc[i]['obscene'] + frame.iloc[i][
+            'threat'] + frame.iloc[i]['identity_hate'] + frame.iloc[i]['insult']
         for j in range(len(x)):
             slowo = x[j].lower()
             if slowo != '':
@@ -25,10 +26,11 @@ def create_data():
                     else:
                         dictionary_bad[slowo] = 0
     for key in dictionary.keys():
-        result = str((key, float(dictionary_bad[key]/dictionary[key])))
+        result = str((key, float(dictionary_bad[key] / dictionary[key])))
         file.write(result)
-        print(result)
+        #print(result)
     file.close()
+    print("Długość zapisywanego słownika {}".format(len(dictionary)))
 
 
 def read_data_from_file():
@@ -36,18 +38,20 @@ def read_data_from_file():
     outputNegative = file.read().split(')(')
     print(type(outputNegative))
     outputDict = {}
-    try:
-        for i in range(len(outputNegative)):
-            #za pomocą regexów:
-                #do klucza outputDict dodaj stringa w cudzyszłowie
-                #do wartości dodaj floata
-    except:
-        print('sth went wrong')
-    return outputNegative
+
+    for i in range(len(outputNegative)):
+        temp = outputNegative[i]
+        key = re.search(r"'(.*?)'", temp)
+        variable = float(temp.split(', ')[1][:-1])
+        if key and variable:
+            key = key.group()
+            outputDict[str(key[1:-1])] = variable
+
+    return len(outputDict), outputDict
 
 
-print(read_data_from_file()[7])
-
+print(read_data_from_file())
+#create_dataa()
 
 """
 TODO-
