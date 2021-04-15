f1b09c81ea6da8c2b53f6c1a12ec6c28642535d8
Dawid <dawidklose568@gmail.com>
2021-04-15 14:05:38 +0200

    commitDawK8

--- voting.py
+++ voting.py
@@ -5,7 +5,6 @@ import pandas as pd
 class DataCreating:
     @staticmethod
     def create_data():  # 280 k records processed within 2 hours :D
-        file = open('DictOfNegativeWords.txt', 'w')  # file  to the modification
         frame = pd.read_csv('train.csv', low_memory=True)
         dictionary, dictionary_bad = {}, {}
         toxicitySeries = pd.Series(index=[], dtype=float)
@@ -34,15 +33,12 @@ class DataCreating:
             # file.write(result)
             toxicityRatio = float(dictionary_bad[key] / dictionary[key])
             toxicitySeries[key] = toxicityRatio
-        file.close()    # to deletion
         print("Length of a dictionary {}".format(len(dictionary)))
         toxicitySeries.to_csv('savedWord.csv')
         return toxicitySeries
 
 
 class Voting:
-
-    toxicitySeries = pd.read_csv('savedWord.csv')
 
     @staticmethod
     def voting(sampleSplit):
@@ -51,11 +47,16 @@ class Voting:
         if type(sampleSplit) is not list:
             sampleSplit = (sampleSplit.split(' '))
         lenSampleSplit = len(sampleSplit)
+        print(len(sampleSplit))             # to remove
         #print("co to jest", sampleSplit)    # to remove
-        for i in range(len(sampleSplit)):
+        wrong = 0
+        for i in range(len(sampleSplit)):  # need to find a better way to looking frames (maybe 5-7?)
             if not toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == sampleSplit[i]].empty:
                 counter += float(toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == sampleSplit[i], '0'].item())
-            else: lenSampleSplit -= 1  # setting unknown word as not important
+            else:
+                lenSampleSplit -= 1  # setting unknown word as not important
+                #print('sth wrong (1)')
+                wrong += 1
 
         toxicityOfWord = counter/lenSampleSplit
 
@@ -66,74 +67,69 @@ class Voting:
             result = ("Good sentence with ", toxicityOfWord, "toxicity ratio",
                     " Avg toxicity = ", averageToxicity)
         byBus = Voting.analyse_by_bus(sampleSplit)
-        if byBus is not None:
+        if byBus is not None and byBus > 0.3:
+            print(byBus)
             result = ("Bad sentence with ", byBus, "toxicity ratio",
                     " By local")
+        #print("wrong",wrong)
         return result
-
 
 
     @staticmethod
     def analyse_by_bus(sampleSplit):   # method will be refactored (by using checking_toxicity_from_one_bus(sampleSplit)
         #sample = sample.split(' ')
         dict_of_buses = {}         # not sure if we need it
-        counter, modulo = 0, 0
-
-        if len(sampleSplit) % 3 == 1: modulo = 1
-        elif len(sampleSplit) % 3 == 2: modulo = 2
+        counter = 0
+        modulo = len(sampleSplit) % 3
 
         for i in range(0, len(sampleSplit) - modulo, 3):
             tmp = []
-            tmp.extend((sampleSplit[i], sampleSplit[i+1], sampleSplit[i+2]))
+            tmp.extend((sampleSplit[i], sampleSplit[i + 1], sampleSplit[i + 2]))
             for j in range(len(tmp)):
-                if not toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == tmp[j]].empty:
-                    counter += float(toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == tmp[j], '0'].item())
-            #print(tmp) # to remove
-            if counter > 0.69:   # static threshold to be replaced with heuristic
-                return counter
-            else:
-                counter = 0
+                counter += Voting.checking_toxicity_from_one_bus(sampleSplit, j)
+        if modulo == 0:
+            print('modulo0')
+            #return counter
         if modulo == 1:
             print('modulo1')
-            if not toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == sampleSplit[-1]].empty:
-                counter += float(toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == sampleSplit[-1], '0'].item())
-            if counter > 0.69:  # static threshold to be replaced with heuristic
-                return counter
+            counter += Voting.checking_toxicity_from_one_bus(sampleSplit, -1)  # the last word from the list
+            #return counter
         if modulo == 2:
             print('modulo2')
             for k in range(1, 3):
-                if not toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == sampleSplit[-k]].empty:
-                    counter += float(toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == sampleSplit[-k], '0'].item())
-            if counter > 0.69:  # static threshold to be replaced with heuristic
-                return counter
+                counter += Voting.checking_toxicity_from_one_bus(sampleSplit, -k)  # two last words from the list
+        return counter/len(sampleSplit)
 
-    """@staticmethod    # to improve
-    def checking_toxicity_from_one_bus(sampleSplit):
-        if not toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == sampleSplit].empty:
-            return float(toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == sampleSplit, '0'].item())
-        else: return 0"""
+
+    @staticmethod
+    def checking_toxicity_from_one_bus(sampleSplit, iterator):
+        if not toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == sampleSplit[iterator]].empty:
+            return float(toxicitySeries.loc[toxicitySeries['Unnamed: 0'] == sampleSplit[iterator], '0'].item())
+        else:
+            return 0
+            #print('sth wrong (1)')
 
 
 toxicitySeries = pd.read_csv('savedWord.csv')
 
 
-
 #small tests
 sample = 'i love hamburger'
-sample2 = 'i am really proud of you however i want to love you you honey'
-sample3 = 'i am really proud of you'
-sample4 = 'fuck'
+sample2 = "In my opinion it's not about being good or not good. If I were to say what I esteem the most in life, I would say - people. People, who gave me a helping hand when I was a mess, when I was alone. And what's interesting, the chance meetings are the ones that influence our lives. The point is that when you profess certain values, even those seemingly universal, you may not find any understanding which, let me say, which helps us to develop. I had luck, let me say, because I found it. And I'd like to thank life. I'd like to thank it - life is singing, life is dancing, life is love. Many people ask me the same question, but how do you do that? where does all your happiness come from? And i replay that it's easy, it's cherishing live, that's what makes me build machines today, and tomorrow... who knows, why not, i would dedicate myself to do some community working and i would be, wham, not least... planting .... i mean... carrots."
+sample3 = 'fuck head limey vandal'
+sample4 = "Why these edits made under my username Hardcore my Fan were reverted? tits weren't vandalisms, just closure on some GAs after I voted at New York Dolls FAC. And please don't remove the template from the talk page since I'm retired now"
 
 #print(Voting.analyse_by_bus(sample2))
 
-print("Vulgarity factor: {}\n".format(Voting.voting(sample2)))
-print("Vulgarity factor: {}\n".format(Voting.voting(sample2)))
-print("Vulgarity factor: {}\n".format(Voting.voting(sample3)))
-print("Vulgarity factor: {}\n".format(Voting.voting(sample4)))
+print("Sentence: {}\nVulgarity factor: {}\n".format(sample, Voting.voting(sample.lower())))
+print("Sentence: {}\nVulgarity factor: {}\n".format(sample2, Voting.voting(sample2.lower())))
+print("Sentence: {}\nVulgarity factor: {}\n".format(sample3, Voting.voting(sample3.lower())))
+print("Sentence: {}\nVulgarity factor: {}\n".format(sample4, Voting.voting(sample4.lower())))
 
 
+
+#TODO
 """
-TODO
 bybus - lokalne średnie #FINISHED
 obliczanie treshholdów - trzy treshholdy na sztywno #FINISHED
 
@@ -143,6 +139,6 @@ zmiana lokalnych busów - np na 4, na 5, na 2
 
 Długoterminowo:
 speech to text - zamiana mowy na tekst i sprawdzenie toksyczności (do poszerzenia programu)
-
+translacja na język polski - np google tłumacz plugin
 """
 
